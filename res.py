522991323245a6d0f8385ff7b9a0c5a6506a37a1
SuperCarol <18279059162@163.com>
2019-02-22 23:56:52 -0800

    测试

--- res.py
+++ res.py
@@ -1,1 +1,15 @@
 print('fueguew')
+# list1=[1,4]
+# for i in range(0,10):
+#
+#     if i not in list1:
+#
+#         list1.append(i)
+#         list1.sort()
+#
+# print(list1)
+
+list1 = [1,4,5,3]
+list2 = sorted(list1)
+print(list2)
+print(list1)
